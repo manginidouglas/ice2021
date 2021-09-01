d29680d9818a37c07e9b3c1605d0227bfd502f53
manginidouglas <moreiramangini@gmail.com>
2021-09-01 15:13:58 -0300

    rerun dados pca apos corrigir i223 e i322

--- pca/bind_indicadores.R
+++ pca/bind_indicadores.R
@@ -9,32 +9,38 @@ df_pedro <- list.files("dados_finais/", pattern = "^i\\d{3}.csv$") %>%
   select(id_municipio, sigla_uf, nome, matches("^i\\d{3}$"))
 
 # indicadores do douglas --------------------------------------------------
+# lista os indicadores do douglas e monta o path para le-los
 files_douglas <- list.files("dados_finais/", pattern = "sd") %>%
   paste0("dados_finais/",.) 
 
+# le os indicadores e printa 'erro' ao inves de dar erro
 ind_douglas <- files_douglas %>% 
   map(possibly(read_xlsx, otherwise = "erro"))
 
+# agrupa os indicadores xlsx e seleciona as colunas relevantes
 df_douglas_xlsx <- ind_douglas %>% 
-  keep(is.tibble) %>% 
+  keep(is_tibble) %>% 
   reduce(bind_cols) %>%
   select(id_municipio = 1, sigla_uf = 2, nome = 3, matches("^i\\d{3}$"))
 
+# onde deu erro
 not_tibble <- which(!sapply(ind_douglas,is.tibble))
 
+# onde deu erro mude a extensao para csv
 files_douglas[not_tibble] <- 
   str_replace_all(files_douglas[not_tibble], ".xlsx",".csv")
 
+# agrupa os indicadores csv e seleciona as colunas relevantes
 df_douglas_csv <-  files_douglas[not_tibble] %>% 
   map(read_csv)%>% 
   reduce(bind_cols) %>%
   select(id_municipio = 1, sigla_uf = 2, nome = 3, matches("^i\\d{3}$"))
 
 
+
 df_douglas <- bind_cols(df_douglas_csv,df_douglas_xlsx) %>%
   select(1,2, 3,matches("^i\\d{3}$")) %>%
   rename(id_municipio = 1, sigla_uf = 2, nome = 3)
-
 
 # tudo junto --------------------------------------------------------------
 
@@ -45,14 +51,25 @@ df <- df_pedro %>%
 indicadores <- names(df) %>% sort() %>% str_subset("^i\\d{3}$")
 
 df_final <- df %>%
-  select(id_municipio, sigla_uf, nome, indicadores)
+  select(id_municipio, sigla_uf, nome, all_of(indicadores))
 
 df_final_indicadores <- df_final %>% 
   select(matches("^i\\d{3}$")) %>%
   mutate(across(everything(), as.numeric))
 
-
 # testes ------------------------------------------------------------------
+
+# testes indicadores douglas
+df_douglas %>% map(is.na) %>%
+  unlist() %>% sum()
+# nenhum null
+df_douglas %>% map(is.null) %>%
+  unlist() %>% sum()
+# nenhum Inf
+df_douglas %>% map(is.infinite) %>%
+  unlist() %>% sum()
+
+# testes indicadores - geral
 # nenhum NA
 df_final_indicadores %>% map(is.na) %>%
   unlist() %>% sum()
@@ -63,6 +80,7 @@ df_final_indicadores %>% map(is.null) %>%
 df_final_indicadores %>% map(is.infinite) %>%
   unlist() %>% sum()
 
+# salva -------------------------------------------------------------------
 
 write_csv(df_final,"pca/indicadores_completo.csv")
 
